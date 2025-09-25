ef03aad113749b8bda146256848e24dd389200a8
Angel <armasangel193@gmail.com>
2025-09-24 19:32:55 -0600

    Se actuazlizo Screen y NavGraph

--- app/src/main/java/com/uvg/uvgestor/navigation/NavGraph.kt
+++ app/src/main/java/com/uvg/uvgestor/navigation/NavGraph.kt
@@ -1,2 +1,26 @@
 package com.uvg.uvgestor.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.NavHostController
+import com.uvg.uvgestor.ui.screens.*
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable(Screen.Home.route) { HomeScreen(navController) }
+        composable(Screen.List.route) { ListScreen(navController) }
+        composable(Screen.Grid.route) { GridScreen(navController) }
+        composable(route = Screen.Detail.route,
+            arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(itemId = id, navController = navController)
+        }
+        composable(Screen.Profile.route) { ProfileScreen(navController) }
+    }
+}
--- app/src/main/java/com/uvg/uvgestor/navigation/Screen.kt
+++ app/src/main/java/com/uvg/uvgestor/navigation/Screen.kt
@@ -1,2 +1,11 @@
 package com.uvg.uvgestor.navigation
 
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object List : Screen("list")
+    object Grid : Screen("grid")
+    object Detail : Screen("detail/{itemId}") {
+        fun createRoute(itemId: Int) = "detail/$itemId"
+    }
+    object Profile : Screen("profile")
+}
