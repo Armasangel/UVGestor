878df9c3450c2ae6a3766a1e684e43b87a94a030
Ronald <romanuelcatun13@gmail.com>
2025-10-01 20:14:26 -0600

    Update NavGraph.kt

--- app/src/main/java/com/uvg/uvgestor/navigation/NavGraph.kt
+++ app/src/main/java/com/uvg/uvgestor/navigation/NavGraph.kt
@@ -16,7 +16,5 @@ fun NavGraph(navController: NavHostController) {
             val id = backStackEntry.arguments?.getString("id")?.toIntOrNull() ?: 0
             TransactionDetailScreen(id, navController)
         }
-        composable(Screen.FinancialAdvice.route) { FinancialAdviceScreen(navController) }
-        composable(Screen.AddExpense.route) { AddExpenseScreen(navController) }
     }
 }
